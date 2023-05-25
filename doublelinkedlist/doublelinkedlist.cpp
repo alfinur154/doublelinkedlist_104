06641a108f299e1309b25cdf243d2cf9d613042e
alfinur154 <alfinur27@gmail.com>
2023-05-25 16:07:23 +0700

    menambah kondisi newNode

--- doublelinkedlist/doublelinkedlist.cpp
+++ doublelinkedlist/doublelinkedlist.cpp
@@ -60,13 +60,19 @@ void DoubleLinkedlist::addNode() {
     while (current->next != NULL && current->next->noMhs < nim)  //step 1.c
     {
         previous = current;             // step 1.d
-        current = current->; //step 1.e
+        current = current->next; //step 1.e
     }
 
     if (current->next != NULL && nim == current->next->noMhs) {
         cout << "\nDuplicate roll numbers not allowed" << endl;
         return;
     }
+
+    newNode->next = current->next;          //step 4
+    newNode->prev = current;        //step 5
+    if (current->next != NULL)
+        current->next->prev = newNode;      //step 6
+    current->next = newNode;            //step 7
 
 }
 int main()
